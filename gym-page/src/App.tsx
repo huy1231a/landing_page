1d369981bc6f74b19a46a840b25c5b43fc580f9a
nQHuy2 <67337070+huy1231a@users.noreply.github.com>
2023-04-20 15:02:46 +0000

    add layout

---
+++ gym-page/src/App.tsx
@@ -0,0 +1,12 @@
+import Nav from "./components/Nav";
+
+
+function App() {
+  return (
+    <div className="app bg-blue-400"> 
+      <Nav />
+    </div>
+  )
+}
+
+export default App;
---
+++ gym-page/src/components/Nav/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div className='icon fixed top-0 z-30 w-full py-6'>
+        <div className='icon mx-auto w-5/6'>
+            <p className='cursor-pointer'>Home</p>
+            <p>About</p>
+        </div>
+    </div>
+  )
+}
+
+export default index
